60258e516bb296ab54a7d54de528be714abbcdf0
김정은 <jungrlo@gimjeong-eun-ui-MacBookPro.local>
2024-01-14 00:10:58 +0900

    feat: 체중수정/PatchWeightRequest.java, WeightResponse.java 생성

--- src/main/java/mealplanb/server/dto/weight/WeightResponse.java
+++ src/main/java/mealplanb/server/dto/weight/WeightResponse.java
@@ -1,18 +1,16 @@
 package mealplanb.server.dto.weight;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
 
 @Getter
-@Setter
-@NoArgsConstructor
-public class PostWeightResponse {
+@AllArgsConstructor
+public class WeightResponse {
 
-    /** 체중 등록 */
+    /** 체중 수정 */
     private double weight;
     private LocalDate date;
 }
