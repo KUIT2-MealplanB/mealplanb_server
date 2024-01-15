6d4c9d5d0d27971a3e0bbd3a626d52f23abfcd5d
lee-june-young <uhiccup@gmail.com>
2024-01-15 20:58:37 +0900

    feat: PostNewFoodReq&Res 구현

---
+++ src/main/java/mealplanb/server/dto/food/PostNewFoodRequest.java
@@ -0,0 +1,25 @@
+package mealplanb.server.dto.food;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostNewFoodRequest {
+    /** 사용자로부터 입력받은 식사 데이터베이스에 등록 */
+
+    private String name;
+    private String category;
+    private int quantity;
+    private double kcal;
+    private double carbohydrate;
+    private double protein;
+    private double fat;
+    private double sugar;
+    private double natrium;
+    private double cholesterol;
+    private double saturatedFattyAcid;
+    private double transFatAcid;
+}
