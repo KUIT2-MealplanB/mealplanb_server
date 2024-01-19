a2672c9a9803e1c11f4f143f339c69ec31f03565
lee-june-young <uhiccup@gmail.com>
2024-01-19 14:32:52 +0900

    feat: WeightRepository & WeightRepositoryTest

---
+++ src/main/java/mealplanb/server/repository/WeightRepository.java
@@ -0,0 +1,8 @@
+package mealplanb.server.repository;
+
+import mealplanb.server.domain.Weight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeightRepository extends JpaRepository<Weight, Long> {
+
+}
