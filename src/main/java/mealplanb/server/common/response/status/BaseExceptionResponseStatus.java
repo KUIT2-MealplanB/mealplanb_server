168e05ddf62c1fcff8cfe836a8c13885a876bda5
jungrlo <kje2066@naver.com>
2024-01-21 00:03:39 +0900

    feat: MEMBER_NOT_FOUND 추가

--- src/main/java/mealplanb/server/common/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/mealplanb/server/common/response/status/BaseExceptionResponseStatus.java
@@ -34,8 +34,12 @@ public enum BaseExceptionResponseStatus implements ResponseStatus {
     INVALID_TOKEN(4003, HttpStatus.UNAUTHORIZED.value(), "유효하지 않은 토큰입니다."),
     MALFORMED_TOKEN(4004, HttpStatus.UNAUTHORIZED.value(), "토큰이 올바르게 구성되지 않았습니다."),
     EXPIRED_TOKEN(4005, HttpStatus.UNAUTHORIZED.value(), "만료된 토큰입니다."),
-    TOKEN_MISMATCH(4006, HttpStatus.UNAUTHORIZED.value(), "로그인 정보가 토큰 정보와 일치하지 않습니다.");
+    TOKEN_MISMATCH(4006, HttpStatus.UNAUTHORIZED.value(), "로그인 정보가 토큰 정보와 일치하지 않습니다."),
 
+    /**
+     * 5000: Member 오류
+     */
+    MEMBER_NOT_FOUND(5000, HttpStatus.NOT_FOUND.value(), "회원을 찾을 수 없습니다.");
 
     private final int code;
     private final int status;
