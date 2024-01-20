d47c75580488600a789c539a379eadf627fe2ef6
jungrlo <kje2066@naver.com>
2024-01-21 00:50:13 +0900

    feat: 사용자 id로 멤버 찾는 메소드 생성

--- src/main/java/mealplanb/server/repository/MemberRepository.java
+++ src/main/java/mealplanb/server/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package mealplanb.server.repository;
 import mealplanb.server.domain.Member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    // 사용자 id 로 멤버를 찾는 메소드
+    Optional<Member> findById(Long memberId);
 }
