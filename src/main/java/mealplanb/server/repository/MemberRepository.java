02af286f15759a4762fcde6f85b776283247f9a5
jungrlo <kje2066@naver.com>
2024-01-21 22:58:39 +0900

    feat: 이메일로 중복확인 추가

--- src/main/java/mealplanb/server/repository/MemberRepository.java
+++ src/main/java/mealplanb/server/repository/MemberRepository.java
@@ -3,10 +3,11 @@ package mealplanb.server.repository;
 import mealplanb.server.domain.Member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email); // 이메일로 유저 찾기
-
+    boolean existsByEmail(String email); // 이메일 중복 확인
 }
