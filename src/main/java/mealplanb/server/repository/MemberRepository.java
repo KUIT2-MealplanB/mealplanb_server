65f4ae5512d2e9d2d3b6ca9ac4e0bd1170f6507a
jungrlo <kje2066@naver.com>
2024-01-21 21:30:03 +0900

    feat: findByEmail 생성

--- src/main/java/mealplanb/server/repository/MemberRepository.java
+++ src/main/java/mealplanb/server/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package mealplanb.server.repository;
 import mealplanb.server.domain.Member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email); // 이메일로 유저 찾기
 
 }
