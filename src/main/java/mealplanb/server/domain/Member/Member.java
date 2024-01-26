28e1f907d0ffe56c78c1aae334d58440fa6e817d
jungrlo <kje2066@naver.com>
2024-01-26 16:14:18 +0900

    fix: @Tostring 애노테이션 누락

--- src/main/java/mealplanb/server/domain/Member/Member.java
+++ src/main/java/mealplanb/server/domain/Member/Member.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import mealplanb.server.domain.*;
 import mealplanb.server.domain.Base.BaseTimeEntity;
 import org.springframework.data.annotation.CreatedBy;
