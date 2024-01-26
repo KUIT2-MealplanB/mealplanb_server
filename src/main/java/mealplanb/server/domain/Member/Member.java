ad7cf7c7b3c8f3f26c18ed0d3694986124245b71
jungrlo <kje2066@naver.com>
2024-01-26 16:50:14 +0900

    fix: 깃 오류 수정

--- src/main/java/mealplanb/server/domain/Member/Member.java
+++ src/main/java/mealplanb/server/domain/Member/Member.java
@@ -4,9 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import mealplanb.server.domain.*;
 import mealplanb.server.domain.Base.BaseTimeEntity;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDate;
