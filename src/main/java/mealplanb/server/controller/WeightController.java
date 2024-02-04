b7c14008842c93dcafa266bb77004dac6fc81f5b
lee-june-young <uhiccup@gmail.com>
2024-02-05 01:21:56 +0900

    feat: WeightController.getDailyWeight 구현

--- src/main/java/mealplanb/server/controller/WeightController.java
+++ src/main/java/mealplanb/server/controller/WeightController.java
@@ -3,6 +3,8 @@ package mealplanb.server.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mealplanb.server.common.response.BaseResponse;
+import mealplanb.server.dto.weight.GetWeightStatisticResponse;
+import mealplanb.server.dto.weight.GetWeightStatisticResponse.DailyWeightResponse;
 import mealplanb.server.dto.weight.WeightResponse;
 import mealplanb.server.service.WeightService;
 import mealplanb.server.util.jwt.JwtProvider;
@@ -28,5 +30,15 @@ public class WeightController {
         log.info("[WeightController.getTodayWeight]");
         Long memberId = jwtProvider.extractIdFromHeader(authorization);
         return new BaseResponse<>(weightService.getTodayWeight(memberId));
+    }
+
+    /**
+     * 체중 일간 조회
+     */
+    @GetMapping("/daily")
+    public BaseResponse<DailyWeightResponse> getDailyWeight(@RequestHeader("Authorization") String authorization){
+        log.info("[WeightController.getDailyWeight]");
+        Long memberId = jwtProvider.extractIdFromHeader(authorization);
+        return new BaseResponse<>(weightService.getDailyWeight(memberId));
     }
 }
