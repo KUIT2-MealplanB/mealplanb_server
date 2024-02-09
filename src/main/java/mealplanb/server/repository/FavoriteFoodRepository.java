571e636df64b3abb4f662f9f3bde1f0a86a4256f
jungrlo <kje2066@naver.com>
2024-02-10 00:26:42 +0900

    feat: findByMember_MemberIdAndFood_FoodIdAndStatus 생성

--- src/main/java/mealplanb/server/repository/FavoriteFoodRepository.java
+++ src/main/java/mealplanb/server/repository/FavoriteFoodRepository.java
@@ -11,4 +11,5 @@ public interface FavoriteFoodRepository extends JpaRepository<FavoriteFood, Long
 
     boolean existsByFood_FoodIdAndMember_MemberIdAndStatus(long foodId, long memberId, BaseStatus a);
     Optional<List<FavoriteFood>> findByMember_MemberIdAndStatus(long memberId, BaseStatus A);
+    Optional<FavoriteFood> findByMember_MemberIdAndFood_FoodIdAndStatus(long memberId, long foodId, BaseStatus A);
 }
