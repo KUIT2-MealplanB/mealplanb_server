2e8105d2d27c315aa5d7c93e35acc0225dbb2bdf
jungrlo <kje2066@naver.com>
2024-02-09 23:39:50 +0900

    feat: FavoriteFoodRepository 에 findByMember_MemberIdAndStatus 생성

--- src/main/java/mealplanb/server/repository/FavoriteFoodRepository.java
+++ src/main/java/mealplanb/server/repository/FavoriteFoodRepository.java
@@ -4,7 +4,11 @@ import mealplanb.server.domain.Base.BaseStatus;
 import mealplanb.server.domain.FavoriteFood;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface FavoriteFoodRepository extends JpaRepository<FavoriteFood, Long> {
 
     boolean existsByFood_FoodIdAndMember_MemberIdAndStatus(long foodId, long memberId, BaseStatus a);
+    Optional<List<FavoriteFood>> findByMember_MemberIdAndStatus(long memberId, BaseStatus A);
 }
