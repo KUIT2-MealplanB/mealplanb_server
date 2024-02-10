482c286c3697a2cd9a86e2799c00ff5358dda6fb
jungrlo <kje2066@naver.com>
2024-02-10 23:12:33 +0900

    feat: FavoriteMealRepository 에 existsByFavoriteMealNameAndStatus 생성

--- src/main/java/mealplanb/server/repository/FavoriteMealRepository.java
+++ src/main/java/mealplanb/server/repository/FavoriteMealRepository.java
@@ -1,8 +1,10 @@
 package mealplanb.server.repository;
 
+import mealplanb.server.domain.Base.BaseStatus;
 import mealplanb.server.domain.FavoriteMeal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FavoriteMealRepository extends JpaRepository<FavoriteMeal, Long> {
 
+    boolean existsByFavoriteMealNameAndStatus(String favoriteMealName, BaseStatus A);
 }
