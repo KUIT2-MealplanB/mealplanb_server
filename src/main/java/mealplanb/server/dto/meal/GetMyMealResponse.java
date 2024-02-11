e2b63f75c36c7db797b5dfe10571e866380e5525
jungrlo <kje2066@naver.com>
2024-02-12 00:58:01 +0900

    feat: GetMyMeqlResponse 생성

---
+++ src/main/java/mealplanb/server/dto/meal/GetMyMealResponse.java
@@ -0,0 +1,32 @@
+package mealplanb.server.dto.meal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class GetMyMealResponse {
+    /**
+     * 나의 식단 조회
+     */
+    private List<FavoriteMealItem> mealItemList;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class FavoriteMealItem{
+        private long favoriteMealId;
+        private String favoriteMealName;
+        private int mealKcal;
+
+        public FavoriteMealItem(Long favoriteMealId, String favoriteMealName, int totalKcal) {
+            this.favoriteMealId = favoriteMealId;
+            this.favoriteMealName = favoriteMealName;
+            this.mealKcal = totalKcal;
+        }
+    }
+}
