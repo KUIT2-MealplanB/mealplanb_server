0efb1724e495a226e08d096c0d5af991ca9e7e41
jungrlo <kje2066@naver.com>
2024-02-12 01:37:03 +0900

    feat: 나의 식단 삭제 구현

--- src/main/java/mealplanb/server/controller/FavoriteMealController.java
+++ src/main/java/mealplanb/server/controller/FavoriteMealController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import mealplanb.server.common.response.BaseResponse;
 import mealplanb.server.dto.meal.GetMyMealResponse;
 import mealplanb.server.dto.meal.PostMyMealRequest;
+import mealplanb.server.service.FavoriteMealComponentService;
 import mealplanb.server.service.FavoriteMealService;
 import mealplanb.server.util.jwt.JwtProvider;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,7 @@ public class FavoriteMealController {
 
     private final JwtProvider jwtProvider;
     private final FavoriteMealService favoriteMealService;
+    private final FavoriteMealComponentService favoriteMealComponentService;
 
     /**
      * 나의 식단 등록
@@ -39,5 +41,19 @@ public class FavoriteMealController {
         log.info("[FavoriteMealController.getMyMeal]");
         Long memberId = jwtProvider.extractIdFromHeader(authorization);
         return new BaseResponse<>(favoriteMealService.getMyMeal(memberId));
+    }
+
+    /**
+     * 나의 식단 삭제
+     */
+
+    @PatchMapping("")
+    public BaseResponse<Void> deleteMyMeal(@RequestHeader("Authorization") String authorization,
+                                           @PathVariable Long favoriteMealId){
+        log.info("[FavoriteMealController.deleteMyMeal]");
+        Long memberId = jwtProvider.extractIdFromHeader(authorization);
+        favoriteMealService.deleteMyMeal(memberId, favoriteMealId);
+        favoriteMealComponentService.deleteMyMealComponent(favoriteMealId);
+        return new BaseResponse<>(null);
     }
 }
