42d099d671c995634c42cc13bf24c09660b0dbd5
jungrlo <kje2066@naver.com>
2024-02-12 01:36:56 +0900

    feat: 나의 식단 삭제 레포지토리 코드 구현

--- src/main/java/mealplanb/server/repository/FavoriteMealComponentRepository.java
+++ src/main/java/mealplanb/server/repository/FavoriteMealComponentRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 
 public interface FavoriteMealComponentRepository extends JpaRepository<FavoriteMealComponent, Long> {
     Optional<List<FavoriteMealComponent>> findByFavoriteMeal_FavoriteMealIdAndStatus(Long favoriteMealId, BaseStatus A);
+    Optional<FavoriteMealComponent> findOneByFavoriteMeal_FavoriteMealIdAndStatus(Long favoriteMealId, BaseStatus A);
 }
--- src/main/java/mealplanb/server/repository/FavoriteMealRepository.java
+++ src/main/java/mealplanb/server/repository/FavoriteMealRepository.java
@@ -11,4 +11,5 @@ public interface FavoriteMealRepository extends JpaRepository<FavoriteMeal, Long
 
     boolean existsByFavoriteMealNameAndStatus(String favoriteMealName, BaseStatus A);
     Optional<List<FavoriteMeal>> findByMember_MemberIdAndStatus(Long memberId, BaseStatus A);
+    Optional<FavoriteMeal> findByMember_MemberIdAndFavoriteMealIdAndStatus(Long memberId, Long favoriteMealID, BaseStatus A);
 }
