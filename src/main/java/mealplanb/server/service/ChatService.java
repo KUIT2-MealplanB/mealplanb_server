ab346930b2c8736165a055721a758799e6a5256a
lee-june-young <uhiccup@gmail.com>
2024-02-13 13:27:46 +0900

    feat: ChatService.getAmountSuggestion

--- src/main/java/mealplanb/server/service/ChatService.java
+++ src/main/java/mealplanb/server/service/ChatService.java
@@ -2,6 +2,7 @@ package mealplanb.server.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import mealplanb.server.dto.chat.GetAmountSuggestionResponse;
 import mealplanb.server.dto.chat.GetCheatDayFoodResponse;
 import mealplanb.server.dto.chat.GetCheatDayFoodResponse.cheatDayFoodInfo;
 import org.springframework.stereotype.Service;
@@ -24,5 +25,14 @@ public class ChatService {
         String lackingNutrientName = memberService.getLackingNutrientName(memberId);
         List<cheatDayFoodInfo> cheatDayFood = foodService.getCheatDayFood(remainingKcal, lackingNutrientName, category);
         return new GetCheatDayFoodResponse(cheatDayFood);
+    }
+
+    /**
+     * 얼마나 먹을까요
+     */
+    public GetAmountSuggestionResponse getAmountSuggestion(Long memberId, Long foodId) {
+        log.info("[ChatService.getAmountSuggestion]");
+        int remainingKcal = memberService.calculateRemainingKcal(memberId);
+        return foodService.getAmountSuggestion(remainingKcal, foodId);
     }
 }
